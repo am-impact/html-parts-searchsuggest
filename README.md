652169dfdd66dad31a6f10fbc9d12c6c7c151c90
Niek Weevers <niekweevers@gmail.com>
2015-02-04 16:38:19 +0100

    Aanpassing nav nieuwe manier in Craft
    Searchsuggest template wordt meegestuurd aan amnav plugin

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ en [html parts search](https://github.com/am-impact/html-parts-search)
  * scss/components/_searchsuggest.scss
  * kit/includes/_searchsuggest.kit
  * scripts/fw.searchsuggest.js
+ 
+## Zelf regelen
+FW.Config.searchUrl: '{{ actionUrl("amSearch/getResults", { template: "searchresult/search", sections: "product" })|raw }}'
+
+Uit de ajaxrequest komt html: een lijst met li's. In de Craft startup is deze standaard toegevoegd
--- scripts/fw.searchsuggest.js
+++ scripts/fw.searchsuggest.js
@@ -1,120 +1,79 @@
 /**
- * Zoek suggesties tijdens typen
- *  - minder dan 3 karakters: zoeksuggesties leeg en verbergen
- *  - meer dan 3 karakters: met ajax suggesties ophalen en tonen wanneer resultaten
- *  - meer dan 3 karakters als resultaten er al zijn: filteren door de verkregen ajax result
+ * FW.Config.searchUrl: '{{ actionUrl("amSearch/getResults", { template: "searchresult/search", sections: "product" })|raw }}'
+ * Uit de ajaxrequest komt html: een lijst met li's. In de Craft startup is deze standaard toegevoegd
  */
 (function() {
-    var $form = $('.search__form'),
+    var $search = $('.search'),
+        $form = $('.search__form'),
         $input = $form.find('input[type=text]'),
         $suggest = $('.searchsuggest'),
         $message = $('searchsuggest__message'),
         $loader = $suggest.find('.loader'),
         $suggestButton = $suggest.find('input[type=submit]'),
+        xhr,
 
-        suggestsVisible = false,
-        suggestsRetrieved = false,
+        keyTimeout = 600,
+        timeoutInput,
 
-        maxLengthSuggests = 10,
-        lengthForSearch = 3,
-        searchUrl = FW.Config.submap + 'searchresult/search?search=',
-        template = '<li data-search="{{searchstring}}"><a href="{{url}}">{{titel}}</a></li>';
-
-    //
-    // Hide suggest
-    //
-    function hideSuggest() {
-        $suggest.find('a:focus').blur();
-        $suggest.addClass('visuallyhidden');
-        $message.addClass('hidden');
-        suggestsVisible = false;
-    }
-
-    //
-    // Show suggest
-    //
-    function showSuggest() {
-        $suggest.removeClass('visuallyhidden');
-        suggestsVisible = true;
-    }
+        minLengthQuery = 3,
+        searchQuery = '';
 
     //
     // Suggest leeg gooien
     //
     function emptySuggest() {
         $suggest.find('ul').empty();
-
-        // deze weer op false zetten zodat deze weer opgehaald kunnen worden
-        suggestsRetrieved = false;
     }
 
     //
-    // Get suggest
+    // Get Results
     //
-    function getSuggest( searchvalue ) {
-        $.getJSON( searchUrl + searchvalue, function( data ) {
-            var items = [];
+    function getResults( val ) {
+        if( xhr && xhr.readyState != 4 ) {
+            xhr.abort();
+        }
 
-            $loader.addClass('hidden');
-
-            if( data.length > 0 ) {
-                $.each( data, function( key, val ) {
-                    var tplhtml = template
-                                    .replace( /{{searchstring}}/ig, val.searchstring.toLowerCase() )
-                                    .replace( /{{url}}/ig, val.url )
-                                    .replace( /{{titel}}/ig, val.titel )
-                    items.push( tplhtml );
-                });
-
-                $('.search__form').find('ul').html( items.join('') );
-                showSuggest();
-
-                // Even voor de zekerheid filteren op huidige waarde in zoekveld
-                // omdat misschien de bezoeker al heeft doorgetypt terwijl het resultaat nog niet is opgehaald
-                filterSuggest( $input.val() );
+        xhr = $.ajax({
+            url: FW.Config.searchUrl,
+            data: { 'searchQuery': val }
+        }).done(function(data) {
+            if( data.html.length < 9 ) {
+                $message.removeClass('hidden');
             }
-        }).fail(function() {
-            $loader.addClass('hidden');
-            $message.removeClass('hidden');
+            else {
+                $message.addClass('hidden');
+            }
 
-            // Resultaten ontvangen op true zetten
-            suggestsRetrieved = false;
+            $search.find('ul').html( data.html );
+            $suggest.removeClass('visuallyhidden');
+            $loader.addClass('hidden');
         });
     }
 
     //
-    // Filter suggest
+    // Hide suggest
     //
-    function filterSuggest( searchvalue ) {
-            // Eerst spaties uit zoek input filteren
-            // Als er bijvoorbeeld gezocht wordt op Voornaam Achternaam, dan wordt ook het juiste resultaat gevonden
-        var searchvalue = searchvalue.replace(/\s+/g, ''),
-            $matches = $suggest.find('li[data-search*="' + searchvalue.toLowerCase() + '"]'),
-            total_matches = $matches.length;
-
-        $suggest.find('li').addClass('visuallyhidden');
-
-        if( total_matches > 0 ) {
-            $matches.filter(':lt(' + maxLengthSuggests + ')').removeClass('visuallyhidden');
-
-            if( !$message.hasClass('hidden') ) {
-                $message.addClass('hidden');
-            }
-        }
-        else {
-            if( $message.hasClass('hidden') && suggestsRetrieved ) {
-                $message.removeClass('hidden');
-            }
-        }
-
-        total_matches > maxLengthSuggests ? $suggestButton.show() : $suggestButton.hide();
+    function hideSuggest() {
+        $suggest.find('a:focus').blur();
+        emptySuggest();
+        $suggest.addClass('visuallyhidden');
+        $message.addClass('hidden');
     }
 
+    //
+    // Show suggest
+    //
+    function showSuggest() {
+        if( $suggest.hasClass('visuallyhidden') ) {
+            $suggest.removeClass('visuallyhidden');
+        }
+    }
 
     //
     // Focus
     //
     function switchFocus( dir, keyevent ) {
+        if( $loader.is(':visible') ) return;
 
         var input_focus_and_suggest_visible = $input.is(':focus') && !$suggest.hasClass('visuallyhidden');
 
@@ -145,7 +104,7 @@
                     }
 
                     // Eerste item? Dan zoekveld
-                    if( $focused_item.length > 0 && $prev_item.length == 0 ) {
+                    if( $focused_item.length > 0 && $prev_item.length === 0 ) {
                         $input.focus();
                         return;
                     }
@@ -173,7 +132,7 @@
                     }
 
                     // Laatste item? dan 'alle' focussen
-                    if( $focused_item.length > 0 && $next_item.length == 0 ) {
+                    if( $focused_item.length > 0 && $next_item.length === 0 ) {
                         if( $suggestButton.is(':visible') ) {
                             $suggestButton.focus();
                         }
@@ -195,54 +154,33 @@
         }
     }
 
-
     //
-    // Acties bij typen
+    // Input event
     //
-    $input.on('keyup', function() {
+    $input.on('keyup', function(e) {
+        if( timeoutInput ) clearTimeout(timeoutInput);
 
-        var search_input = $(this).val();
+        if( $input.val() == searchQuery ) return;
 
-        // Value kleiner dan 'lengthForSearch'
-        if( search_input.length < lengthForSearch ) {
-            // Als de resultaten zichtbaar zijn, dan verbergen
-            if( suggestsVisible ) { hideSuggest(); }
+        searchQuery = $input.val();
 
-            // Als er resultaten zijn, dan deze leeggooien
-            if( suggestsRetrieved ) { emptySuggest(); }
+        if( searchQuery.length < minLengthQuery ) {
+            hideSuggest();
         }
-
-        // Value gelijk aan 'lengthForSearch'
-        if( search_input.length >= lengthForSearch ) {
-            $form.removeClass('search__form--error');
-
-            // Als er al resultaten zijn, bijvoorbeeld na een backspace vanaf lengte 4, dan alleen filteren
-            if( suggestsRetrieved ) { filterSuggest( search_input ); }
-
-            // Als er nog geen resultaten zijn, dan deze met ajax ophalen
-            if( !suggestsRetrieved ) {
+        else {
+            showSuggest();
+            timeoutInput = setTimeout(function() {
                 $loader.removeClass('hidden');
-                $message.addClass('hidden');
-                showSuggest();
-
-                getSuggest( search_input );
-
-                // Resultaten ontvangen op true zetten
-                suggestsRetrieved = true;
-            }
-        }
-
-        // Value groter dan 'lengthForSearch'
-        // Alleen uitvoeren als er daadwerkelijk resultaten uit de ajax call zijn gekomen
-        if( search_input.length > lengthForSearch && suggestsRetrieved ) {
-            filterSuggest( search_input );
-
-            // Als de resultaten niet zichtbaar zijn, dan deze zichtbaar maken
-            if( !suggestsVisible ) { showSuggest(); }
+                getResults( searchQuery );
+            }, keyTimeout);
         }
     });
 
-    $suggest.on('click', '.searchsuggest__close', hideSuggest);
+    $input.on('focus', function() {
+        $input.select();
+    });
+
+    $suggest.on('click', '.searchform__close', hideSuggest);
 
     //
     // Escape op keyup uitvoeren omdat de input ook met keyup werkt
@@ -252,6 +190,7 @@
             // Escape
             case 27:
                 hideSuggest();
+                $input.select();
                 break;
         }
     });
